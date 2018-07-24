e76fd913aa6476e9603a936033f6e0671d10231a
gareth <garethsimons@me.com>
2018-07-24 16:03:31 +0100

    updated dependencies

--- dist/vue-mapbox-feature.esm.js
+++ dist/vue-mapbox-feature.esm.js
@@ -335,9 +335,10 @@ var script = {
     // important for cleaning up old layers and avoiding style clashes
     this.cleanup();
   }
-}
+};
 
-var __vue_script__ = script;
+/* script */
+            var __vue_script__ = script;
             
 /* template */
 var __vue_render__ = function() {
@@ -349,138 +350,136 @@ var __vue_render__ = function() {
 var __vue_staticRenderFns__ = [];
 __vue_render__._withStripped = true;
 
-var __vue_template__ = typeof __vue_render__ !== 'undefined'
-  ? { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ }
-  : {};
-/* style */
-var __vue_inject_styles__ = function (inject) {
-  if (!inject) { return }
-  inject("data-v-3f72160a_0", { source: "\n/*# sourceMappingURL=VueMapboxFeature.vue.map */", map: undefined, media: undefined });
+  /* style */
+  var __vue_inject_styles__ = function (inject) {
+    if (!inject) { return }
+    inject("data-v-3f72160a_0", { source: "\n/*# sourceMappingURL=VueMapboxFeature.vue.map */", map: {"version":3,"sources":["VueMapboxFeature.vue"],"names":[],"mappings":";AACA,gDAAgD","file":"VueMapboxFeature.vue"}, media: undefined });
 
-};
-/* scoped */
-var __vue_scope_id__ = undefined;
-/* module identifier */
-var __vue_module_identifier__ = undefined;
-/* functional template */
-var __vue_is_functional_template__ = false;
-/* component normalizer */
-function __vue_normalize__(
-  template, style, script$$1,
-  scope, functional, moduleIdentifier,
-  createInjector, createInjectorSSR
-) {
-  var component = script$$1 || {};
+  };
+  /* scoped */
+  var __vue_scope_id__ = undefined;
+  /* module identifier */
+  var __vue_module_identifier__ = undefined;
+  /* functional template */
+  var __vue_is_functional_template__ = false;
+  /* component normalizer */
+  function __vue_normalize__(
+    template, style, script$$1,
+    scope, functional, moduleIdentifier,
+    createInjector, createInjectorSSR
+  ) {
+    var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
 
-  {
-    component.__file = "/Users/gareth/dev/github/vue-mapbox-feature/src/VueMapboxFeature.vue";
-  }
-
-  if (!component.render) {
-    component.render = template.render;
-    component.staticRenderFns = template.staticRenderFns;
-    component._compiled = true;
-
-    if (functional) { component.functional = true; }
-  }
-
-  component._scopeId = scope;
-
-  {
-    var hook;
-    if (style) {
-      hook = function(context) {
-        style.call(this, createInjector(context));
-      };
+    {
+      component.__file = "/Users/gareth/dev/github/vue-mapbox-feature/src/VueMapboxFeature.vue";
     }
 
-    if (hook !== undefined) {
-      if (component.functional) {
-        // register for functional component in vue file
-        var originalRender = component.render;
-        component.render = function renderWithStyleInjection(h, context) {
-          hook.call(context);
-          return originalRender(h, context)
+    if (!component.render) {
+      component.render = template.render;
+      component.staticRenderFns = template.staticRenderFns;
+      component._compiled = true;
+
+      if (functional) { component.functional = true; }
+    }
+
+    component._scopeId = scope;
+
+    {
+      var hook;
+      if (style) {
+        hook = function(context) {
+          style.call(this, createInjector(context));
         };
-      } else {
-        // inject component registration as beforeCreate hook
-        var existing = component.beforeCreate;
-        component.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+      }
+
+      if (hook !== undefined) {
+        if (component.functional) {
+          // register for functional component in vue file
+          var originalRender = component.render;
+          component.render = function renderWithStyleInjection(h, context) {
+            hook.call(context);
+            return originalRender(h, context)
+          };
+        } else {
+          // inject component registration as beforeCreate hook
+          var existing = component.beforeCreate;
+          component.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
       }
     }
+
+    return component
   }
+  /* style inject */
+  function __vue_create_injector__() {
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
+    var isOldIE =
+      typeof navigator !== 'undefined' &&
+      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
 
-  return component
-}
-/* style inject */
-function __vue_create_injector__() {
-  var head = document.head || document.getElementsByTagName('head')[0];
-  var styles = {};
-  var isOldIE =
-    typeof navigator !== 'undefined' &&
-    /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+    return function addStyle(id, css) {
+      if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) { return } // SSR styles are present.
 
-  return function addStyle(id, css) {
-    if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) { return } // SSR styles are present.
+      var group = isOldIE ? css.media || 'default' : id;
+      var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
 
-    var group = isOldIE ? css.media || 'default' : id;
-    var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
+      if (!style.ids.includes(id)) {
+        var code = css.source;
+        var index = style.ids.length;
 
-    if (!style.ids.includes(id)) {
-      var code = css.source;
-      var index = style.ids.length;
+        style.ids.push(id);
 
-      style.ids.push(id);
-
-      if (isOldIE) {
-        style.element = style.element || document.querySelector('style[data-group=' + group + ']');
-      }
-
-      if (!style.element) {
-        var el = style.element = document.createElement('style');
-        el.type = 'text/css';
-
-        if (css.media) { el.setAttribute('media', css.media); }
         if (isOldIE) {
-          el.setAttribute('data-group', group);
-          el.setAttribute('data-next-index', '0');
+          style.element = style.element || document.querySelector('style[data-group=' + group + ']');
         }
 
-        head.appendChild(el);
-      }
+        if (!style.element) {
+          var el = style.element = document.createElement('style');
+          el.type = 'text/css';
 
-      if (isOldIE) {
-        index = parseInt(style.element.getAttribute('data-next-index'));
-        style.element.setAttribute('data-next-index', index + 1);
-      }
+          if (css.media) { el.setAttribute('media', css.media); }
+          if (isOldIE) {
+            el.setAttribute('data-group', group);
+            el.setAttribute('data-next-index', '0');
+          }
 
-      if (style.element.styleSheet) {
-        style.parts.push(code);
-        style.element.styleSheet.cssText = style.parts
-          .filter(Boolean)
-          .join('\n');
-      } else {
-        var textNode = document.createTextNode(code);
-        var nodes = style.element.childNodes;
-        if (nodes[index]) { style.element.removeChild(nodes[index]); }
-        if (nodes.length) { style.element.insertBefore(textNode, nodes[index]); }
-        else { style.element.appendChild(textNode); }
+          head.appendChild(el);
+        }
+
+        if (isOldIE) {
+          index = parseInt(style.element.getAttribute('data-next-index'));
+          style.element.setAttribute('data-next-index', index + 1);
+        }
+
+        if (style.element.styleSheet) {
+          style.parts.push(code);
+          style.element.styleSheet.cssText = style.parts
+            .filter(Boolean)
+            .join('\n');
+        } else {
+          var textNode = document.createTextNode(code);
+          var nodes = style.element.childNodes;
+          if (nodes[index]) { style.element.removeChild(nodes[index]); }
+          if (nodes.length) { style.element.insertBefore(textNode, nodes[index]); }
+          else { style.element.appendChild(textNode); }
+        }
       }
     }
   }
-}
-/* style inject SSR */
+  /* style inject SSR */
+  
 
-
-var VueMapboxFeature = __vue_normalize__(
-  __vue_template__,
-  __vue_inject_styles__,
-  typeof __vue_script__ === 'undefined' ? {} : __vue_script__,
-  __vue_scope_id__,
-  __vue_is_functional_template__,
-  __vue_module_identifier__,
-  typeof __vue_create_injector__ !== 'undefined' ? __vue_create_injector__ : function () {},
-  typeof __vue_create_injector_ssr__ !== 'undefined' ? __vue_create_injector_ssr__ : function () {}
-)
+  
+  var VueMapboxFeature = __vue_normalize__(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    __vue_create_injector__,
+    undefined
+  );
 
 export default VueMapboxFeature;
