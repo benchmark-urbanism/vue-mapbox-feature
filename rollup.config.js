862b7c3efe01a682d788e44f8f665c1491eb95cb
gareth <garethsimons@me.com>
2019-03-06 15:19:58 +0000

    updated dependencies / version bumps

--- rollup.config.js
+++ rollup.config.js
@@ -1,67 +1,48 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import vue from 'rollup-plugin-vue'
 import buble from 'rollup-plugin-buble'
 import pkg from './package.json' // import names from package to reduce errors
 
 // see https://github.com/rollup/rollup-starter-lib/blob/buble/rollup.config.js
-export default [
-  // browser friendly UMD build
-  {
-    input: 'src/index.js',
-    external: [ 'es6-tween' ],
-    output: {
-      name: 'VueMapboxFeature',
+export default {
+  input: 'src/index.js',
+  external: [ 'es6-tween' ],
+  output: [
+    {
+      name: 'VueMapboxMap',
       file: pkg.browser,
-      format: 'umd',
+      format: 'umd', // browser friendly UMD build
       globals: {
         'es6-tween': 'TWEEN'
       }
     },
-    plugins: [
-      vue({
-        compileTemplate: true, // true by default
-        css: true
-      }),
-      buble({
-        exclude: ['node_modules/**'],
-        transforms: {
-          dangerousForOf: true
-        }
-      })
-    ]
-  },
-  // CommonJS (for Node) and ES module (for bundlers) build
-  {
-    input: 'src/index.js',
-    external: [ 'es6-tween' ], // suppresses warnings about external modules
-    output: [
-      {
-        name: 'VueMapboxFeature',
-        file: pkg.main,
-        format: 'cjs',
-        globals: {
-          'es6-tween': 'TWEEN'
-        }
-      },
-      {
-        name: 'VueMapboxFeature',
-        file: pkg.module,
-        format: 'es',
-        globals: {
-          'es6-tween': 'TWEEN'
-        }
+    {
+      name: 'VueMapboxFeature',
+      file: pkg.main,
+      format: 'cjs', // CommonJS (for Node)
+      globals: {
+        'es6-tween': 'TWEEN'
       }
-    ],
-    plugins: [
-      vue({
-        compileTemplate: true, // true by default
-        css: true
-      }),
-      buble({
-        exclude: ['node_modules/**'],
-        transforms: {
-          dangerousForOf: true
-        }
-      })
-    ]
-  }
-]
+    },
+    {
+      name: 'VueMapboxFeature',
+      file: pkg.module,
+      format: 'es', // ES module (for bundlers)
+      globals: {
+        'es6-tween': 'TWEEN'
+      }
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    vue({
+      css: true
+    }),
+    buble({
+      transforms: { forOf: false },
+      exclude: ['node_modules/**']
+    })
+  ]
+}
