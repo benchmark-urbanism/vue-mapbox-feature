14b4d6ae3d2311b44f0fa6082492100fd7effdcb
Gareth Simons <garethsimons@me.com>
2021-05-08 20:33:15 +0100

    fixes IDE configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:vue/recommended'],
   plugins: ['prettier', 'vue'],
   parser: 'vue-eslint-parser',
-  parserOptions: {
-    ecmaVersion: 2020
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 }
